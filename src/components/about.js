7ba60bcbc17c7f1163a6cc712e34dadfe0623d44
Nesrine1333 <nesrine.ncibi@polytechnicien.tn>
2025-10-01 19:02:06 +0100

    fixing/upcoming-events

--- src/components/about.js
+++ src/components/about.js
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 export default function About() {
   return (
-    <div id="about" className="md:max-lg:px-10  sm:max-md:px-10 sm:px-10 lg:px-40  lg:max-xl:px-40 max-xl:px-100 max-sm:py-10 lg:max-xl:pt-40 lg:pt-40 md:max-lg:py-40
-    sm:max-md:py-15 pb-0 flex flex-col items-center justify-center  ">
-      <section className="  align-center flex items-center max-md:text-center max-sm:text-center sm:text-center  md:text-center lg:justify-center w-full max-md:w-100 max-sm:w-100 lg:max-xl:w-100 lg:w-80 ">
-        <div className=" ">
+    <div id="about" className=" md:max-lg:px-20  sm:max-md:px-10 sm:px-10 lg:px-40  lg:max-xl:px-22  max-sm:py-10 lg:max-xl:pt-40 lg:pt-40 md:max-lg:py-40
+    sm:max-md:py-4 pb-0 flex flex-col items-center justify-center  ">
+      <section className="  align-center flex justify-center items-center max-md:text-center max-sm:text-center sm:text-center  md:text-center lg:justify-center w-full max-md:w-100 max-sm:w-100 lg:max-xl:w-100 lg:w-80 ">
+        <div className=" lex flex-col gap-4 py-2 ">
           <h1 className="fontheader lg:font-bold font-color lg:mb-7 text-start lg:text-6xl md:max-lg:text-4xl lg:max-xl:text-6xl  max-sm:text-sm sm:max-md:text-3xl">
            What Is IEEE WIE ESSTHS AG
           </h1>
@@ -17,7 +17,7 @@ export default function About() {
         </div>
         <div className="  flex items-center justify-center">
             <div className="relative lg:max-xl:h-120 lg:max-xl:w-80
-            max-xl:w-80 lg:h-120 lg:w-70 md:h-100 md:w-50 md:max-lg:h-100 md:max-lg:w-50 sm:w-50 sm:h-60 max-sm:w-20 max-sm:h-60 max-md:w-50 max-md:h-60 flex items-center justify-center  ">
+            max-xl:w-80 lg:h-120 lg:w-70 md:h-100 md:w-50 md:max-lg:h-100 md:max-lg:w-50 sm:w-50 sm:h-80 max-sm:w-30 max-sm:h-50 max-md:w-50 max-md:h-80 flex items-center justify-center  ">
                             <div
                               className="flex items-center justify-center"
                               style={{ position: 'relative', width: '100%', height: '100%' }}
